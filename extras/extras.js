13cbe0c9c97d0b7538d4d136e4b9194604c4087c
Faustine <73058621+FauW1@users.noreply.github.com>
2022-07-06 21:59:47 +0000

    attempted to merge using destructured assignment

--- extras/extras.js
+++ extras/extras.js
@@ -27,3 +27,21 @@ const serverSettings = (guild, callCenter, mods, activePlayer) => {
 };
 
 module.exports = serverSettings;
+
+// NOTE: An embed OBJECT!! different from builder
+const fawntuneEmbed = {
+	color: 0x0824deb,
+	title: 'Hope you enjoy my bot!',
+	description: 'Check out more of my work and consider supporting me! :)',
+  thumbnail: {
+    url: "https://i.imgur.com/J0w3MWF.png",
+  },
+	fields: [
+		{ name: 'Support me on Ko-Fi', value: 'https://ko-fi.com/fawntune' },
+		{ name: 'Instagram', value: 'https://www.instagram.com/faustine.art/', inline: true },
+		{ name: 'Itch.io', value: 'https://fawntune.itch.io/', inline: true },
+    { name: 'Linktree', value: 'https://linktr.ee/faustinew'},
+	],
+};
+
+module.exports = fawntuneEmbed;
